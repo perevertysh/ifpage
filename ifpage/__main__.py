8531f0e4862851dc7444b5043b44d11ab92b80a0
perevertysh <none@none.none>
2019-09-01 02:49:09 +0300

    fix path remove dir_name

--- ifpage/__main__.py
+++ ifpage/__main__.py
@@ -11,12 +11,10 @@ def set_parser():
     parser.add_argument('--url', type=str,  help='page address')
     parser.add_argument('--pattern', default="", type=str, help='r-string pattern to re.findall()')
     parser.add_argument('--ext', type=str, help='file extension')
-    parser.add_argument('--dir_name', default="samples", type=str, help='target dir name')
+
     parser.add_argument('--dir_path', type=str, help='path to target directory')
     return parser.parse_args()
-
-
-def fetch_imgs(dir_name="", dir_path="", ext="", url="", pattern=""):
+def fetch_imgs(dir_path="", ext="", url="", pattern=""):
     if ext:
         pattern = r"[\"(](http[s]?://[^\s]{0,150}." + "{})[\")]".format(ext)
 
@@ -24,16 +22,13 @@ def fetch_imgs(dir_name="", dir_path="", ext="", url="", pattern=""):
                            method='GET').content.decode('utf-8')
     pprint.pprint(res)
     links = re.findall(pattern, res)
-    os.system("mkdir {}".format(dir_name))
-    os.system("chmod 777 {}".format(dir_name))
     counter = 0
     for link in links:
         print(link)
+        path = os.path.abspath(os.path.dirname(__file__))
         if dir_path:
-            path = dir_path
-        else:
-            path = os.path.abspath("".join(os.path.dirname(__file__).split("/")[0:-2]))
-        filename = "{}{}img{}.{}".format(path, "/" + dir_name + "/" if dir_name else "", counter, ext)
+            path = dir_path 
+        filename = "{}/img{}.{}".format(path, counter, ext)
         os.system("touch {}".format(filename))
         with open(filename, 'wb') as file:
             file.write(requests.request(url=link, method='GET').content)
@@ -46,8 +41,7 @@ def main():
 
         fetch_imgs(url=args.url,
                    pattern=args.pattern if args.pattern else None,
-                   ext=args.ext,
-                   dir_name=args.dir_name,
+                   ext=args.ext,         
                    dir_path=args.dir_path)
         print("params of fetch:")
         [print("{}={}".format(key, value)) for key, value in args.__dict__.items()]
